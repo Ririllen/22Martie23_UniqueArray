127499c3968d6893cd6b14a15d2ca89df5047d9d
Elena Teaca <ririteaca@gmail.com>
2023-03-22 18:38:02 +0200

    upgrade la code

--- UniqueArray.js
+++ UniqueArray.js
@@ -3,14 +3,19 @@
 
 const eliminareDuplicate = (nums) => {
     let result = [];
-
-    for (const num of nums){
-        if (result.includes(num) === false){
+    // for (const num of nums){
+    //     if (result.includes(num) === false){
+    //         result.push(num);
+    //     }
+    // }
+    nums.forEach(num => {
+        if (!result.includes(num)){
             result.push(num);
-        }
-    }
+        }        
+    });
     return result;
 }
 
 
-console.log(eliminareDuplicate([1,2,3,4,1,2,6,8,2,7,3]));
+console.log(eliminareDuplicate([1,2,3,4,-1,2,6,-8,8,2,7,3,-3]));
+console.log(eliminareDuplicate([]));
